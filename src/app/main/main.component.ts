4a09db456247e7299e98922baf6408ab48c993b9
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-02-15 13:49:35 +0100

    addet all main components into html

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { SecondaryChatComponent } from './secondary-chat/secondary-chat.component';
+import { MainChatComponent } from './main-chat/main-chat.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SideBarComponent, MainChatComponent, SecondaryChatComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
