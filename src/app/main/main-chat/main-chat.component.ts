b767120f1c34a796d669ed9259cce30d1838e83d
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-02-15 21:03:05 +0100

    main chat

--- src/app/main/main-chat/main-chat.component.ts
+++ src/app/main/main-chat/main-chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-chat',
@@ -8,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './main-chat.component.scss'
 })
 export class MainChatComponent {
+    channels = [{
+      'id': 'sijfef8e8',
+      'name': 'Entwicklerteam',
+      'members': [{
+        'userId': 'sadf123sadf',
+        'name': 'Tobias',
+        'surname': 'Odermatt',
+        'photo': '../../../assets/img/member1.svg'
+      },
+      {
+        'userId': 'iej896sdf',
+        'name': 'Pierce',
+        'surname': 'C.',
+        'photo': '../../../assets/img/member2.svg'
+      },
+      {
+        'userId': 'sadmvkui25ddf',
+        'name': 'Filip',
+        'surname': 'Todoroski',
+        'photo': '../../../assets/img/member3.svg'
+      }
+      ]
+    }];
 
+    membercount = this.channels[0]['members'].length;
+
+    constructor(private common: CommonModule) {
+
+    }
+  
 }
