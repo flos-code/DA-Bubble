3724e83a90b8aa5e1d39d41077930242b741048e
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-02-15 13:37:11 +0100

    update

---
+++ src/app/main/secondary-chat/secondary-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './secondary-chat.component.html',
+  styleUrl: './secondary-chat.component.scss'
+})
+export class SecondaryChatComponent {
+
+}
