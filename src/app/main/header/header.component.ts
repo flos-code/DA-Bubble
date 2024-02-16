0a3e8cf426aa97243a7eb1c5f91afdaa1d99d916
paclem <pascal.moeller@posteo.de>
2024-02-16 13:17:45 +0100

    added dropdown menu profil

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+// import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  // imports: [MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isOverlayActive: boolean = false;
 
+  constructor() { }
+
+  menuItemClicked(option: string) {
+    console.log('Option clicked:', option);
+  }
+
+  toggleOverlay(active: boolean) {
+    this.isOverlayActive = active;
+  }
 }
