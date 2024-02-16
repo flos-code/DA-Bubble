d31857c30a63d1ec9ad313b78373115705ddf710
flos-code <148456982+flos-code@users.noreply.github.com>
2024-02-16 13:51:47 +0100

    channels section in sidebar

--- src/app/main/side-bar/side-bar.component.ts
+++ src/app/main/side-bar/side-bar.component.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss',
 })
-export class SideBarComponent {}
+export class SideBarComponent {
+  channels: string[] = ['Allgemein', 'Entwicklerteam', 'Office-team'];
+  users = [
+    {
+      firstName: 'Frederik',
+      lastName: 'Beck',
+      email: 'f.beck@mail.com',
+      onlineStatus: 'active',
+      image: 'src/assets/img/userImages/userImage1.svg',
+      isYou: false,
+      userID: 0,
+    },
+    {
+      firstName: 'Sofia',
+      lastName: 'Müller',
+      email: 's.müller@mail.com',
+      onlineStatus: 'offline',
+      image: 'src/assets/img/userImages/userImage2.svg',
+      isYou: true,
+      userID: 1,
+    },
+    {
+      firstName: 'Noah',
+      lastName: 'Braun',
+      email: 'n.braun@mail.com',
+      onlineStatus: 'away',
+      image: 'src/assets/img/userImages/userImage3.svg',
+      isYou: false,
+      userID: 2,
+    },
+  ];
+}
