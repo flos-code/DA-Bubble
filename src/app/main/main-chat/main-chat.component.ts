ed67b65ca417ceca8066c207df05a7c908591a32
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-02-16 19:39:15 +0100

    main chat

--- src/app/main/main-chat/main-chat.component.ts
+++ src/app/main/main-chat/main-chat.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-chat',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './main-chat.component.html',
   styleUrl: './main-chat.component.scss'
 })
 export class MainChatComponent {
+    addMemberDialogOpen = false;
+    newMember: string = "";
+    newMemberObject = {
+      'userId': 'ikeikeoie',
+      'name': this.newMember,
+      'surname': 'M.',
+      'photo': '../../../assets/img/main-chat/member2.svg'
+    };
+
     channels = [{
       'id': 'sijfef8e8',
       'name': 'Entwicklerteam',
@@ -33,10 +43,55 @@ export class MainChatComponent {
       ]
     }];
 
+    users = [{
+      'userId': 'sadmvkui25ddf',
+      'name': 'Filip',
+      'surname': 'Todoroski',
+      'photo': '../../../assets/img/main-chat/member1.svg'
+    },
+    {        'userId': 'sadf123sadf',
+    'name': 'Tobias',
+    'surname': 'Odermatt',
+    'photo': '../../../assets/img/main-chat/member2.svg'
+    },
+    {
+      'userId': 'iej896sdf',
+      'name': 'Pierce',
+      'surname': 'C.',
+      'photo': '../../../assets/img/main-chat/member3.svg'
+    },
+    {
+      'userId': 'okokloilk366',
+      'name': 'Pascal',
+      'surname': 'M.',
+      'photo': '../../../assets/img/main-chat/member1.svg'
+    },
+    {
+      'userId': 'sadfsadf8585',
+      'name': 'Florian',
+      'surname': 'Scholz',
+      'photo': '../../../assets/img/main-chat/member2.svg'
+    },  
+  ];
+
     membercount = this.channels[0]['members'].length;
 
-    constructor(private common: CommonModule) {
+    constructor() { }
 
+    toggleAddMemberDialog() {
+      if(this.addMemberDialogOpen == false) {
+        this.addMemberDialogOpen = true;
+      } else {
+        this.addMemberDialogOpen = false;
+      }
+    }
+
+    closeAddMemberDialog() {
+        this.addMemberDialogOpen = false;
+    }
+
+    addMember() {
+      this.channels[0].members.push(this.newMemberObject);
     }
   
 }
