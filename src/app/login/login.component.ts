a237049e8f915fc440714cf393d76144e189f229
Filip Todoroski <nabrzinka@gmail.com>
2024-02-16 17:48:00 +0100

    added firebase auth, sign in with email + password, sign in with google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC520Za3P8qTUGvWM0KxuYqGIMaz-Vd48k",
+  authDomain: "da-bubble-87fea.firebaseapp.com",
+  projectId: "da-bubble-87fea",
+  storageBucket: "da-bubble-87fea.appspot.com",
+  messagingSenderId: "970901942782",
+  appId: "1:970901942782:web:56b67253649b6206f290af"
+};
+
+const provider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig);
 
 @Component({
   selector: 'app-login',
@@ -9,18 +23,48 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  auth = getAuth(app);
   constructor() { }
 
 
 
   validateEmail() {
+    let email = (<HTMLInputElement>document.getElementById('login_email')).value;
     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let inputValue = (<HTMLInputElement>document.getElementById('login_email')).value;
-    if (inputValue.match(mailformat)) {
+    if (email.match(mailformat)) {
       return true;
     }
     else {
       return false;
     }
+  }
+
+  async signInWithGoogle() {
+    await signInWithPopup(this.auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    // const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+   }
+
+  async signIn() {
+    let email = (<HTMLInputElement>document.getElementById('login_email')).value;
+    let password = (<HTMLInputElement>document.getElementById('login_password')).value;
+    const userCredentials = await signInWithEmailAndPassword(this.auth, email, password);
+    console.log(userCredentials.user.uid);
   }
 }
