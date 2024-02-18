33116f717dcae0f08af7f90864a0c381a1778144
flos-code <148456982+flos-code@users.noreply.github.com>
2024-02-18 14:14:41 +0100

    text-box component styled

--- src/app/main/new-message/text-box/text-box.component.ts
+++ src/app/main/new-message/text-box/text-box.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-text-box',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, FormsModule],
   templateUrl: './text-box.component.html',
-  styleUrl: './text-box.component.scss'
+  styleUrl: './text-box.component.scss',
 })
 export class TextBoxComponent {
+  inputFocused: boolean = false;
+  messageModel: string = '';
 
+  onInputFocus(): void {
+    this.inputFocused = true;
+  }
+
+  onInputBlur(): void {
+    this.inputFocused = false;
+  }
 }
