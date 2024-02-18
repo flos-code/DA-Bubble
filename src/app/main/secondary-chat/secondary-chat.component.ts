02f511a7ed21d768999cac8040a78ced1718471a
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-02-18 20:31:47 +0100

    first emoji tests

--- src/app/main/secondary-chat/secondary-chat.component.ts
+++ src/app/main/secondary-chat/secondary-chat.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+import { EmojiComponent, EmojiEvent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
 
 @Component({
   selector: 'app-secondary-chat',
   standalone: true,
-  imports: [PickerComponent],
+  imports: [PickerComponent, EmojiComponent],
   templateUrl: './secondary-chat.component.html',
   styleUrl: './secondary-chat.component.scss'
 })
 export class SecondaryChatComponent {
+  themes = ['native', 'apple', 'google', 'twitter', 'facebook'];
+  darkMode: undefined | boolean = !!(
+    typeof matchMedia === 'function' && matchMedia('(prefers-color-scheme: dark)').matches
+  );
+  darkestMode: undefined | boolean = undefined;
+  set = 'native';
+  native = true;
+
   messages = [
     {
       id: 1,
@@ -34,4 +43,16 @@ export class SecondaryChatComponent {
   ];
 
   constructor() { }
+
+  handleClick($event: EmojiEvent) {
+    console.log($event.emoji);
+  }
+
+  emojiFilter(e: string): boolean {
+    // Can use this to test [emojisToShowFilter]
+    if (e && e.indexOf && e.indexOf('1F4') >= 0) {
+      return true;
+    }
+    return false;
+  }
 }
