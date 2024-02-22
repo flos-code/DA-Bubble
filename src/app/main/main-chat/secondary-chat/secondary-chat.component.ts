f8b764f0ad435a44692c438c4468b630a3b5fed5
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-02-22 18:05:07 +0100

    insert emoji in textarea

--- src/app/main/main-chat/secondary-chat/secondary-chat.component.ts
+++ src/app/main/main-chat/secondary-chat/secondary-chat.component.ts
@@ -1,19 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 import { EmojiComponent, EmojiEvent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
 import { ChatService } from '../../../services/chat.service';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, onSnapshot,  limit, query, doc, getDoc, updateDoc } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+import { FormsModule } from '@angular/forms';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC520Za3P8qTUGvWM0KxuYqGIMaz-Vd48k",
+  authDomain: "da-bubble-87fea.firebaseapp.com",
+  projectId: "da-bubble-87fea",
+  storageBucket: "da-bubble-87fea.appspot.com",
+  messagingSenderId: "970901942782",
+  appId: "1:970901942782:web:56b67253649b6206f290af"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 @Component({
   selector: 'app-secondary-chat',
   standalone: true,
-  imports: [PickerComponent, EmojiComponent, CommonModule],
+  imports: [PickerComponent, EmojiComponent, CommonModule, FormsModule],
   templateUrl: './secondary-chat.component.html',
   styleUrl: './secondary-chat.component.scss'
 })
 export class SecondaryChatComponent { 
+  @ViewChild('message') messageInput: ElementRef<HTMLInputElement>;
   emojiWindowOpen = false;
+  inputFocused: boolean = false;
   threadOpen: boolean = true;
+  messageModel: string = '';
 
   messages = [
     {
@@ -42,16 +61,17 @@ export class SecondaryChatComponent {
 
   constructor(private chatService: ChatService) { }
 
-  handleClick($event: EmojiEvent) {
-    console.log($event.emoji);
+  onInputFocus(): void {
+    this.inputFocused = true;
   }
 
-  emojiFilter(e: string): boolean {
-    // Can use this to test [emojisToShowFilter]
-    if (e && e.indexOf && e.indexOf('1F4') >= 0) {
-      return true;
-    }
-    return false;
+  onInputBlur(): void {
+    this.inputFocused = false;
+  }
+
+  handleClick(event: any) {
+    const emoji = event.emoji.native;
+    this.insertEmojiAtCursor(emoji);
   }
 
   toggleEmojis() {
@@ -61,6 +81,23 @@ export class SecondaryChatComponent {
       this.emojiWindowOpen = true;
     }
   }
+
+  insertEmojiAtCursor(emoji: string) {
+    const inputEl = this.messageInput.nativeElement;
+    const start = inputEl.selectionStart;
+    const end = inputEl.selectionEnd;
+    const text = inputEl.value;
+    const before = text.substring(0, start);
+    const after = text.substring(end, text.length);
+    this.messageModel = before + emoji + after;
+
+    const newPos = start + emoji.length;
+    setTimeout(() => {
+      inputEl.selectionStart = inputEl.selectionEnd = newPos;
+    });
+  }
+  
+  
 
   closeThread(): void {
     this.chatService.closeThread();
