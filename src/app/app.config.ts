bd0c490512e832a00f10502993fc3d45afc2b0dd
flos-code <148456982+flos-code@users.noreply.github.com>
2024-02-29 16:11:05 +0100

    firebase storage added to uploade images

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC520Za3P8qTUGvWM0KxuYqGIMaz-Vd48k',
+  authDomain: 'da-bubble-87fea.firebaseapp.com',
+  projectId: 'da-bubble-87fea',
+  storageBucket: 'da-bubble-87fea.appspot.com',
+  messagingSenderId: '970901942782',
+  appId: '1:970901942782:web:56b67253649b6206f290af',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideStorage(() => getStorage()),
+    ]),
+  ],
 };
--- src/app/main/new-message/text-box/text-box.component.ts
+++ src/app/main/new-message/text-box/text-box.component.ts
@@ -1,9 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  inject,
+  signal,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 import { EmojiComponent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
+import {
+  Storage,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-text-box',
@@ -23,6 +36,8 @@ export class TextBoxComponent {
   inputFocused: boolean = false;
   messageModel: string = '';
   showEmojiPicker: boolean = false;
+
+  imageURL = signal<string | undefined>(undefined);
 
   onInputFocus(): void {
     this.inputFocused = true;
@@ -66,5 +81,26 @@ export class TextBoxComponent {
     const textarea: HTMLTextAreaElement = event.target;
     textarea.style.height = 'auto'; // Temporarily shrink to auto to get the correct scrollHeight
     textarea.style.height = `${textarea.scrollHeight}px`; // Set the height to match content
+  }
+
+  storage = inject(Storage);
+
+  async onFileSelected(event) {
+    const file: File = event.target.files[0];
+    if (file) {
+      await this.uploadImage(file);
+    }
+  }
+
+  async uploadImage(file: File) {
+    try {
+      const filePath = `myimages/${file.name}`; // Create a path for the file
+      const storageRef = ref(this.storage, filePath);
+      const uploadTask = await uploadBytes(storageRef, file);
+      const downloadUrl = await getDownloadURL(uploadTask.ref);
+      this.imageURL.set(downloadUrl); // Set the image URL to display it
+    } catch (error) {
+      console.error('Error uploading file: ', error);
+    }
   }
 }
