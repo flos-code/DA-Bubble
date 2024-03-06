4defa10ac9ad146e8a82fce8193337525157bf73
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-03-06 21:24:53 +0100

    add reaction to firebase

--- src/app/main/main-chat/reaction-emoji-input/reaction-emoji-input.component.ts
+++ src/app/main/main-chat/reaction-emoji-input/reaction-emoji-input.component.ts
@@ -174,7 +174,7 @@ export class ReactionEmojiInputComponent implements OnInit {
  
 
   async addReaction(emoji: string, currentUser: string) {
-      let newReaction = await addDoc(collection(db, `channels/allgemein/threads/${this.threadId}/reactions`), {
+      let newReaction = await addDoc(collection(db, this.reactionCollectionPath), {
         count: 1,
         reaction: emoji,
         reactedBy: [currentUser],
