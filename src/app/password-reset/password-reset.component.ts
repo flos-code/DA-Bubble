5064d03710d4806fbeed2cda202d1ada90921b39
Filip Todoroski <nabrzinka@gmail.com>
2024-03-10 00:49:41 +0100

    implemented firebase auth. integrated password reset functionality

--- src/app/password-reset/password-reset.component.ts
+++ src/app/password-reset/password-reset.component.ts
@@ -1,7 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { initializeApp } from "firebase/app";
 import { Router } from '@angular/router';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC520Za3P8qTUGvWM0KxuYqGIMaz-Vd48k",
+  authDomain: "da-bubble-87fea.firebaseapp.com",
+  projectId: "da-bubble-87fea",
+  storageBucket: "da-bubble-87fea.appspot.com",
+  messagingSenderId: "970901942782",
+  appId: "1:970901942782:web:56b67253649b6206f290af"
+};
+
+const app = initializeApp(firebaseConfig);
+
 
 @Component({
   selector: 'app-password-reset',
@@ -13,6 +27,7 @@ import { Router } from '@angular/router';
 export class PasswordResetComponent {
 
   constructor(private router: Router, private fb: FormBuilder) { }
+  auth = getAuth(app);
 
   first: boolean = true;
   emailSent: boolean = false;
@@ -25,7 +40,18 @@ export class PasswordResetComponent {
     this.router.navigateByUrl('login');
   }
 
-  sendEmail(){
+  async sendEmail() {
+
+    sendPasswordResetEmail(this.auth, this.emailForPasswordResetForm.value.email)
+      .then(() => {
+        // Password reset email sent!
+        // ..
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
     this.sendEmailBtnDisabled = true;
     this.emailSent = true;
     setTimeout(() => {
