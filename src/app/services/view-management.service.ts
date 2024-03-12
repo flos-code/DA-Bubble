8233dba6e2c59e0f22d5b791908c1dcbff1625ee
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-03-12 16:23:03 +0100

    update

--- src/app/services/view-management.service.ts
+++ src/app/services/view-management.service.ts
@@ -10,10 +10,25 @@ export class ViewManagementService {
   >('showMainChat');
   currentView$ = this.currentView.asObservable();
 
-  constructor() {}
+  private currentMobileView = new BehaviorSubject<
+    'sidebar' | 'mainchat' | 'secondarychat'
+  >('mainchat');
+  currentMobileView$ = this.currentView.asObservable();
+
+  constructor() { }
 
   changeView(view: 'showMainChat' | 'showDms' | 'showNewMessage'): void {
     this.currentView.next(view);
+  }
+
+  changeMobileView(channelSelected: boolean, threadSelected: boolean): void {
+    if (threadSelected) {
+      this.currentMobileView.next('secondarychat');
+    } else if (channelSelected) {
+      this.currentMobileView.next('mainchat');
+    } else {
+      this.currentMobileView.next('sidebar');
+    }
   }
 
   //ausgewählte userId oder channelId muss hier übertragen werden um in jeweiligen
