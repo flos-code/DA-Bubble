4369166f86536a0037a24716ae57056d27741b63
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-03-16 16:33:52 +0100

    update

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -11,16 +10,5 @@ import { Router } from '@angular/router';
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-  animationState = 'in';
 
-  constructor(
-    private router: Router
-  ) { }
-
-  openLogin() {
-    this.animationState = 'out';
-    setTimeout(() => {
-      this.router.navigate(['/login']);
-    }, 850);
-  }
 }
