119b7363105195f825533eb4c3b0424422b9fc1d
Filip Todoroski <nabrzinka@gmail.com>
2024-03-17 02:46:57 +0100

    added redirect to login if no user is logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC520Za3P8qTUGvWM0KxuYqGIMaz-Vd48k",
+  authDomain: "da-bubble-87fea.firebaseapp.com",
+  projectId: "da-bubble-87fea",
+  storageBucket: "da-bubble-87fea.appspot.com",
+  messagingSenderId: "970901942782",
+  appId: "1:970901942782:web:56b67253649b6206f290af"
+};
+
+const app = initializeApp(firebaseConfig);
 
 @Component({
   selector: 'app-root',
@@ -10,6 +23,19 @@ import { MainComponent } from './main/main.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DA-Bubble';
+auth = getAuth(app);
+  constructor ( private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    if (!this.auth.currentUser){
+      this.router.navigateByUrl('/login')
+    } else {
+      
+    }
+  }
+
 }
