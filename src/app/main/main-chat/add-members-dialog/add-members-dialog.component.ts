21b90683c15d2aacd887b87d7831e11bd1c70317
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2024-03-18 09:48:54 +0100

    main chat

--- src/app/main/main-chat/add-members-dialog/add-members-dialog.component.ts
+++ src/app/main/main-chat/add-members-dialog/add-members-dialog.component.ts
@@ -37,6 +37,12 @@ export class AddMembersDialogComponent implements OnInit {
     this.getUsersToAdd();
   }
 
+  /**
+   * Checks if a user is a member of the current channel. If that is not the case, the user is beeing pushed in the userList 
+   * array, containing user name, user id, online status and the url of the profile img. 
+   * 
+   * @returns 
+   */
   getUsersToAdd() {
     const q = query(collection(this.firestore, 'users'));
     return onSnapshot(q, (list) => {
@@ -55,11 +61,20 @@ export class AddMembersDialogComponent implements OnInit {
     });  
   }
 
+  /**
+   * Sets the searchText variable to the input value und triggers the search() function every time the user enters a character
+   * or number.
+   * @param data - Value/text of the input filed
+   */
   searchKey(data:string) {
     this.searchText = data;
     this.search();
   }
 
+  /**
+   * Depending on the search input, the function returns the users that are not yet members of the channel in a list.
+   * 
+   */
   search() {
     this.filteredUserList = this.userList;
 
@@ -72,16 +87,11 @@ export class AddMembersDialogComponent implements OnInit {
     }
   }
 
-  closeDialog() {
-    this.addMemberDialogOpen = false;
-    this.addMemberDialogOpenChild.emit(this.addMemberDialogOpen);
-    this.inputFocus = false;
-  }
-
-  showUserList() {
-    this.inputFocus = true;
-  }
-
+  /**
+   * With clicking on a returned user, it can be added to a pool (seperate array). A new user object is added in the 
+   * input field.
+   * @param filteredUser - Array with all the filtered according to the search input
+   */
   addUser(filteredUser: any) {
     let existingUser = this.userList.find(user => user.userId == filteredUser.userId);
     let indexOfAddedUser = this.userList.indexOf(existingUser);
@@ -91,6 +101,11 @@ export class AddMembersDialogComponent implements OnInit {
     this.inputFocus = false; 
   }
 
+  /**
+   * With a click an the x symbol, the user that has been added to the pool is beeing removed from it and the array updated.
+   * @param userToAdd - user id of the user in the pool array
+   * @param i - index of the user in the pool array
+   */
   removeAddedUser(userToAdd: any, i: number) {
       this.filteredUserList = this.userList;
       this.userList.push(userToAdd);            // Push to user List array
@@ -100,6 +115,11 @@ export class AddMembersDialogComponent implements OnInit {
       this.inputFocus = false;
   }
 
+  /**
+   * Adds all the user ids, that have been added to the pool to the members array of the current channel in firebase.
+   * It also checks if the user allready exists in the members array. 
+   * After the users have been added, the pool array is beeing emptyed and the dialog closed.
+   */
   async addUsers() {
     for (let i = 0; i < this.newUsersToAdd.length; i++) {
       const user = this.newUsersToAdd[i]['userId'];
@@ -115,10 +135,33 @@ export class AddMembersDialogComponent implements OnInit {
     this.closeDialog();
   }
 
+  /**
+   * Hides/closes the add member component and sets the input focus to false.  
+   */
+  closeDialog() {
+    this.addMemberDialogOpen = false;
+    this.addMemberDialogOpenChild.emit(this.addMemberDialogOpen);
+    this.inputFocus = false;
+  }
+
+  /**
+   * Shows the list of users that are no channel members if the input is focused.
+   */
+  showUserList() {
+    this.inputFocus = true;
+  }
+
+  /**
+   * Closes the list of users that are no channel members if the input is not focused.
+   */
   closeUserList() {
     this.inputFocus = false;
   }
 
+  /**
+   * Prevens an unwanted triggering of a function by clicking on an element.
+   * @param $event 
+   */
   doNotClose($event: any) {
     $event.stopPropagation(); 
   }
