f09934bb84b99cf0dab4edc4b4c2fc97a99b2b3a
Filip Todoroski <nabrzinka@gmail.com>
2024-03-18 16:27:25 +0100

    added login animation visible on entry only, and wrong sign in details alert

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule, NavigationStart } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, updateDoc, getDoc, setDoc, arrayUnion, collection, addDoc } from 'firebase/firestore';
-import { RouterModule } from '@angular/router';
 import { DirectMessage } from '../../models/directMessage.class';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 const firebaseConfig = {
   apiKey: "AIzaSyC520Za3P8qTUGvWM0KxuYqGIMaz-Vd48k",
@@ -29,26 +30,63 @@ const db = getFirestore(app);
   styleUrls: ['./login.component.scss', 'login.component.animations.scss']
 })
 
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   auth = getAuth(app);
 
   startTextAnimation: boolean = false;
   removeDNone: boolean = false;
   removeAnimatedContainer: boolean = false;
   startLogoAnimation: boolean = false;
-  removeMainDiv: boolean = false;
   isText: boolean = false;
   type: string = 'password';
-  eyeIcon: string = 'src/assets/img/visibility_off.svg';
+  runAnimation = true;
+
+  private destroy$ = new Subject<void>();
 
   signInForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
   })
-  constructor(private fb: FormBuilder, private router: Router) { }
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    const animationHasRunBefore = sessionStorage.getItem('animationHasRun');
+
+    if (animationHasRunBefore) {
+      // Animation has been run before, don't run it again
+      this.runAnimation = false;
+    }
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngOnInit(): void {
-    this.startTheAnimation();
+    sessionStorage.setItem('animationHasRun', 'true');
+
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationStart),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((event: NavigationStart) => {
+        if (event.navigationTrigger === 'popstate') {
+          this.runAnimation = false;
+        }
+      });
+
+    this.shouldStartAnimation();
+  }
+
+  shouldStartAnimation() {
+    if (this.runAnimation == true) {
+      this.startTheAnimation()
+    } else {
+      this.removeAnimatedContainer = true;
+    }
   }
 
   startTheAnimation() {
@@ -147,12 +185,21 @@ export class LoginComponent implements OnInit {
   }
 
   async signIn() {
-    await signInWithEmailAndPassword(this.auth, this.signInForm.value.email, this.signInForm.value.password);
-    let userRef = doc(db, "users", this.auth.currentUser.uid)
-    await updateDoc(userRef, {
-      isOnline: true,
+    await signInWithEmailAndPassword(this.auth, this.signInForm.value.email, this.signInForm.value.password).then(async () => {
+      let userRef = doc(db, "users", this.auth.currentUser.uid);
+      await updateDoc(userRef, {
+        isOnline: true,
+      });
+      this.router.navigateByUrl('');
+    }).catch((error) => {
+      if (error.message == 'Firebase: Error (auth/user-not-found).') {
+        this.showFalseLoginAlert();
+      } else {
+
+      }
+
     });
-    this.router.navigateByUrl('');
+
   }
 
   async signInAsGuest() {
@@ -166,8 +213,12 @@ export class LoginComponent implements OnInit {
 
   togglePasswordVisibility() {
     this.isText = !this.isText
-    this.isText ? this.eyeIcon = 'src/assets/img/visibility_on.svg' : this.eyeIcon = 'src/assets/img/visibility_off.svg';
     this.type = this.isText ? "text" : "password";
+  }
+
+  showFalseLoginAlert() {
+    alert('Falscher Nutzername und / oder Passwort. Bitte versuchen Sie es erneut.');
+    this.signInForm.reset();
   }
 
 }
